7acac1b4e30d1dd0c747e1ceb46689138d76221b
SemenHatypov <semyon.khatypov@aviasales.ru>
2021-04-30 21:06:44 +0700

    Add simple plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+COVID_DATA_URL = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
+import pandas as pd
 
+def make_simple_plot(df, location, metric):
+    df.query("location == @location").plot(x="date", y=metric, grid=True, title=f"{location}: {metric}")
+
+if __name__ == "__main__":
+    df = pd.read_csv(COVID_DATA_URL, parse_dates=["date"])
+    print("Hello, world")
+    make_simple_plot(df, location="Russia", metric="new_cases")
