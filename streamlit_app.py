d86631eea3c9b7ffabc2898c8849c32d5fdd7ec4
SemenHatypov <semyon.khatypov@aviasales.ru>
2021-04-30 22:37:41 +0700

    Add import streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 COVID_DATA_URL = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
+import streamlit as st
 import pandas as pd
 
 def make_simple_plot(df, location, metric):
-    df.query("location == @location").plot(x="date", y=metric, grid=True, title=f"{location}: {metric}")
+    plot_df = df.query("location == @location").set_index("date")[metric]
+    st.line_chart(plot_df)
 
 if __name__ == "__main__":
     df = pd.read_csv(COVID_DATA_URL, parse_dates=["date"])
-    print("Hello, world")
+    st.write("Hello, world")
     make_simple_plot(df, location="Russia", metric="new_cases")
